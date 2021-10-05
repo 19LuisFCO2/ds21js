66892b26840248b38f59ced01e7b23a7939f2d4e
19LuisFCO2 <90060042+19LuisFCO2@users.noreply.github.com>
2021-10-05 17:34:56 -0500

    Create complexity.js

---
+++ 1p/2complexity/2do/complexity.js
@@ -0,0 +1,12 @@
+function includesDouble(array){
+  let doubles = {}
+  let i = 0
+  while (i < array.lenght){
+    doubles[array[i]] ='cats are so cool'
+    if (doubles [(array[i])*2] ||doubles [(array[i]) /2 ]){
+      return true
+    }
+    i++
+  }
+  return false
+}
